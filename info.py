a25e87176b5f9d8b223ffe983728d7dbb0e0d6e9
ChetanK107 <117459754+ChetanK107@users.noreply.github.com>
2022-11-07 06:53:48 -0800

    Update info.py

---
+++ info.py
@@ -0,0 +1,23 @@
+#why python is dynamic langauge?
+# 1 easy to read/readability
+# 2 no need to define data type
+#3 easy to learn
+# Data types- integer
+# eg. 1,2,3,
+# float
+# eg decimal(1.1,1.2,1.3)
+# string
+#eg. singleqoute(') doubleqoute("") tripleqoute("'for paragraph)
+#  boolean
+# true or false
+#DATA STRUCTURE- list[] ,tuple() ,dictionary{} ,set{}
+#variable are used to store datatype
+#Rules of variables:
+#1.always start with underscore and alphabate
+#2.variable is very case sensitive
+#3.Space  is not in the name of variable
+#Comments(#):
+# Used as a easy to understand code
+# It will not be execute
+# Easy to readiability
+
